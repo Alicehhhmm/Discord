01dbfb0e9237b31369ae963536d423c715a222df
Norush <a007alpum@gmail.com>
2024-11-13 18:24:42 +0800

    test: use UserButton sign out

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from '@/components/ui/button'
+import { UserButton } from '@clerk/nextjs'
+
 export default function Home() {
     return (
         <>
             <h1 className='text-2xl text-blue-500'>hello netxjs15</h1>
             <Button variant='default'>Button</Button>
+            <UserButton afterSwitchSessionUrl='/' />
         </>
     )
 }
