2f9c309e9b0adddb6de4a875a5f0e25868a262d5
Norush <a007alpum@gmail.com>
2024-11-13 18:07:30 +0800

    style: update auth stypes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,5 @@
-const AuthLayout = () => {
-    return (
-        <div className='bg-red-500 h-full'>
-            <h1>AuthLayout</h1>
-        </div>
-    )
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+    return <div className='w-full h-full flex justify-center items-center bg-green-600 dark:bg-stone-950'>{children}</div>
 }
 
 export default AuthLayout
