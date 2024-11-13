6c708da7c93ac3da3236374a54ebf3aec1de51e3
Norush <a007alpum@gmail.com>
2024-11-13 16:53:21 +0800

    feat:  init project

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+export default function Home() {
+    return (
+        <>
+            <h1 className='text-2xl text-blue-500'>hello netxjs15</h1>
+            <Button variant='default'>Button</Button>
+        </>
+    )
+}
