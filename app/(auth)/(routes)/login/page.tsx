d418eca642a4baee47ddfd9468cff0dbd21301b2
Norush <a007alpum@gmail.com>
2024-11-13 17:45:57 +0800

    refe: add sign up

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-const LoginPage = () => {
-    return (
-        <div className=''>
-            <h1>LoginPage</h1>
-        </div>
-    )
-}
-
-export default LoginPage
--- app/(auth)/(routes)/register/page.tsx
+++
@@ -1,9 +0,0 @@
-const RegisterPage = () => {
-    return (
-        <div className=''>
-            <h1>RegisterPage</h1>
-        </div>
-    )
-}
-
-export default RegisterPage
