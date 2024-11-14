0971f764fee0555da592c563a618f2f030d14e3d
Norush <a007alpum@gmail.com>
2024-11-15 00:46:00 +0800

    fix: use initialProfile

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,4 +1,25 @@
-const SetupPage = () => {
+import { redirect } from 'next/navigation'
+import { initialProfile } from '@/lib/initial-profile'
+import { db } from '@/lib/db'
+
+const SetupPage = async () => {
+    const profile = await initialProfile()
+
+    // 根据profileId获取一个服务
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+    })
+
+    if (server) {
+        return redirect(`/servers/${server.id}`)
+    }
+
     return <div className=''>Create a Server</div>
 }
 
