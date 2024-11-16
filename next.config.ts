08476b0e77044de7be5be63f7f620682f7c15df7
Alicehhhmm <1245500390@qq.com>
2024-11-16 19:16:56 +0800

    fix:  use 'next/image' add remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    /* config options here */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'uploadthing.com',
+            },
+            {
+                protocol: 'https',
+                hostname: 'utfs.io',
+            },
+        ],
+    },
+
 };
 
 export default nextConfig;
