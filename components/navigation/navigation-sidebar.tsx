6bfa23985359bd755ec29b8825e978617a31fdb8
Alicehhhmm <1245500390@qq.com>
2024-11-18 00:13:43 +0800

    fix: add servers map

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,8 @@ import { redirect } from 'next/navigation'
 import { db } from '@/lib/db'
 
 import { NavigationAction } from '@/components/navigation/navigation-action'
+import { Separator } from '@/components/ui/separator'
+import { ScrollArea } from '@radix-ui/react-scroll-area'
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile()
@@ -12,7 +14,7 @@ export const NavigationSidebar = async () => {
     }
 
     // 获取所有频道服务
-    const server = await db.server.findMany({
+    const servers = await db.server.findMany({
         where: {
             members: {
                 some: {
@@ -25,6 +27,12 @@ export const NavigationSidebar = async () => {
     return (
         <div className='space-y-4 flex flex-col items-center h-full w-full text-primary dark:bg-[#1E1F22] py-3'>
             <NavigationAction />
+            <Separator className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 max-auto' />
+            <ScrollArea className='flex-1 w-full'>
+                {servers.map(server => (
+                    <div key={server.id}>{server.name}</div>
+                ))}
+            </ScrollArea>
         </div>
     )
 }
