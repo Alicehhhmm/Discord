bf52ea7b9aa5a688dc4809f2aca7be715a40d589
Alicehhhmm <1245500390@qq.com>
2024-11-21 21:35:39 +0800

    feat:  sidebar navigation-item

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,9 +2,10 @@ import { currentProfile } from '@/lib/current-profile'
 import { redirect } from 'next/navigation'
 import { db } from '@/lib/db'
 
-import { NavigationAction } from '@/components/navigation/navigation-action'
 import { Separator } from '@/components/ui/separator'
 import { ScrollArea } from '@radix-ui/react-scroll-area'
+import { NavigationAction } from '@/components/navigation/navigation-action'
+import { NavigationItem } from '@/components/navigation/navigation-item'
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile()
@@ -30,7 +31,7 @@ export const NavigationSidebar = async () => {
             <Separator className='h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 max-auto' />
             <ScrollArea className='flex-1 w-full'>
                 {servers.map(server => (
-                    <div key={server.id}>{server.name}</div>
+                    <NavigationItem id={server.id} name={server.name} imageUrl={server.imageUrl} />
                 ))}
             </ScrollArea>
         </div>
