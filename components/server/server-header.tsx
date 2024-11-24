757cb86d88243bcc63e026554857110ac7de7601
Alicehhhmm <1245500390@qq.com>
2024-11-24 23:51:51 +0800

    feat:  add server search

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -3,7 +3,13 @@
 import { ServerWithMembersWithProfiles } from '@/typings/type'
 import { MemberRole } from '@prisma/client'
 
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator } from '@/components/ui/dropdown-menu'
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+} from '@/components/ui/dropdown-menu'
 import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from 'lucide-react'
 import { useModal } from '@/hooks/use-modal-store'
 
@@ -64,13 +70,19 @@ export const ServerHeader = ({ server, role }: ServerHeaderProps) => {
                 )}
                 {isModerator && <DropdownMenuSeparator />}
                 {isAdmin && (
-                    <DropdownMenuItem onClick={() => onOpen('deleteServer', { server })} className='text-rose-500 px-3 py-2 text-sm cursor-pointer'>
+                    <DropdownMenuItem
+                        onClick={() => onOpen('deleteServer', { server })}
+                        className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
+                    >
                         Delete Channel
                         <Trash className='w-4 h-4 ml-auto' />
                     </DropdownMenuItem>
                 )}
                 {!isAdmin && (
-                    <DropdownMenuItem onClick={() => onOpen('leaveServer', { server })} className='text-rose-500 px-3 py-2 text-sm cursor-pointer'>
+                    <DropdownMenuItem
+                        onClick={() => onOpen('leaveServer', { server })}
+                        className='text-rose-500 px-3 py-2 text-sm cursor-pointer'
+                    >
                         Leave Server
                         <LogOut className='w-4 h-4 ml-auto' />
                     </DropdownMenuItem>
