3ae82a675f223ced323499deba615d200d508e0d
Alicehhhmm <1245500390@qq.com>
2024-11-25 17:56:40 +0800

    fix:  add channels  btn edit del

--- components/server/server-channel.tsx
+++ components/server/server-channel.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { Hash, Mic, Video } from 'lucide-react'
+import { Edit, Hash, Mic, Trash, Video, Lock } from 'lucide-react'
 
 import { useParams, useRouter } from 'next/navigation'
 import { Channel, ChannelType, Server, MemberRole } from '@prisma/client'
 
 import { cn } from '@/lib/utils'
+import { ActionTooltip } from '@/components/action-tooltip'
 
 interface ServerChannelProps {
     channel: Channel
@@ -45,6 +46,31 @@ export const ServerChannel = ({ channel, server, role }: ServerChannelProps) => 
             >
                 {channel.name}
             </p>
+            {/* 管理员 */}
+            {channel.name !== 'general' && role !== MemberRole.GUEST && (
+                <div className='ml-auto flex items-center gap-x-2'>
+                    <ActionTooltip label='Edit'>
+                        <Edit
+                            className='hidden group-hover:block w-4 h-4 text-zinc-500 
+                          hover:text-zinc-600 dark:text-zinc-400 
+                          dark:hover:text-zinc-300 transition'
+                        />
+                    </ActionTooltip>
+                    <ActionTooltip label='Delete'>
+                        <Trash
+                            className='hidden group-hover:block w-4 h-4 text-zinc-500 
+                          hover:text-zinc-600 dark:text-zinc-400 
+                          dark:hover:text-zinc-300 transition'
+                        />
+                    </ActionTooltip>
+                </div>
+            )}
+            {/* 普通用户 */}
+            {channel.name === 'general' && (
+                <ActionTooltip label='Lock'>
+                    <Lock className='ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400' />
+                </ActionTooltip>
+            )}
         </button>
     )
 }
