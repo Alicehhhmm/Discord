1049b111bc8a3a98149070c68808162bb82d2e79
Alicehhhmm <1245500390@qq.com>
2024-11-26 18:36:44 +0800

    feat: chat haeder finish 🎉

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,5 @@
-import { Hash, Menu } from 'lucide-react'
+import { Hash } from 'lucide-react'
+import { MobileToggle } from '@/components/mobile-toggle'
 
 interface ChatHeaderops {
     serverId: string
@@ -9,7 +10,7 @@ interface ChatHeaderops {
 export const ChatHeader = ({ serverId, name, type }: ChatHeaderops) => {
     return (
         <div className='text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2'>
-            <Menu />
+            <MobileToggle serverId={serverId} />
             {['channels'].includes(type) && <Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />}
             <p className='font-semibold text-md text-black dark:text-white'>{name}</p>
         </div>
