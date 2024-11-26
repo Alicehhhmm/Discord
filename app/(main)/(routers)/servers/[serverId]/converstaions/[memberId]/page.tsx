df1817a923f937e0b4c82fdbc0f04283da126517
Alicehhhmm <1245500390@qq.com>
2024-11-27 00:42:17 +0800

    fix: add MemberIdPage

--- app/(main)/(routers)/servers/[serverId]/converstaions/[memberId]/page.tsx
+++ app/(main)/(routers)/servers/[serverId]/converstaions/[memberId]/page.tsx
@@ -1,7 +1,55 @@
-const MemberIdPage = () => {
+import { auth } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
+
+import { db } from '@/lib/db'
+import { currentProfile } from '@/lib/current-profile'
+import { getOrCreateCoversation } from '@/lib/conversation'
+import { ChatHeader } from '@/components/chat/chat-header'
+
+interface MemberIdPageProps {
+    params: {
+        memberId: string
+        serverId: string
+    }
+}
+const MemberIdPage = async ({ params }: MemberIdPageProps) => {
+    const { redirectToSignIn } = await auth()
+    const profile = await currentProfile()
+
+    const { serverId, memberId } = await params
+
+    if (!profile) {
+        return redirectToSignIn()
+    }
+
+    const currentMember = await db.member.findFirst({
+        where: {
+            serverId,
+            profileId: profile.id,
+        },
+        include: {
+            profile: true,
+        },
+    })
+
+    if (!currentMember) {
+        return redirect('/')
+    }
+
+    const conversation = await getOrCreateCoversation(currentMember.id, memberId)
+
+    if (!conversation) {
+        return redirect(`/services/${serverId}`)
+    }
+
+    const { memberOne, memberTwo } = conversation
+
+    // 通过反选确认另一个对话成员
+    const otherMember = memberOne.profileId === profile.id ? memberTwo : memberOne
+
     return (
-        <div>
-            <h1>MemberIdPage</h1>
+        <div className='bg-white dark:bg-[#313338] flex flex-col h-full'>
+            <ChatHeader type='conversation' serverId={serverId} name={otherMember.profile.name} imageUrl={otherMember.profile.imageUrl} />
         </div>
     )
 }
--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,17 +1,20 @@
 import { Hash } from 'lucide-react'
 import { MobileToggle } from '@/components/mobile-toggle'
+import { UserAvatar } from '@/components/use-avatar'
 
 interface ChatHeaderops {
     serverId: string
     name: string
-    type: 'channels' | 'members'
+    type: 'channel' | 'conversation'
+    imageUrl?: string
 }
 
-export const ChatHeader = ({ serverId, name, type }: ChatHeaderops) => {
+export const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderops) => {
     return (
         <div className='text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2'>
             <MobileToggle serverId={serverId} />
-            {['channels'].includes(type) && <Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />}
+            {['channel'].includes(type) && <Hash className='w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2' />}
+            {['conversation'].includes(type) && <UserAvatar src={imageUrl} className='w-8 h-8 md:w-8 md:h-8 mr-2' />}
             <p className='font-semibold text-md text-black dark:text-white'>{name}</p>
         </div>
     )
