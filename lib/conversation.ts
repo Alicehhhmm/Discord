d2eb2e66c6047cd44c4c3e9df8d06d9d26d76226
Alicehhhmm <1245500390@qq.com>
2024-11-26 23:56:09 +0800

    feat: add  converstaion utils

---
+++ lib/conversation.ts
@@ -0,0 +1,83 @@
+import { db } from '@/lib/db';
+
+
+
+/**
+ * 查找第一个对话
+ * @param memberOneId 
+ * @param memberTwoId 
+ * @returns first conversation
+ */
+const findCoversation = async (memberOneId: string, memberTwoId: string) => {
+    try {
+        return await db.conversation.findFirst({
+            where: {
+                AND: [
+                    { memberOneId: memberOneId },
+                    { memberTwoId: memberTwoId },
+                ]
+            },
+            include: {
+                memmberOne: {
+                    include: {
+                        profile: true
+                    }
+                },
+                memmberTwo: {
+                    include: {
+                        profile: true
+                    }
+                }
+            }
+        });
+    } catch {
+        return null;
+    }
+}
+
+/**
+ * 创建成员对话
+ * @param memberOneId 
+ * @param memberTwoId 
+ * @returns new conversation
+ */
+const createNewCoversation = async (memberOneId: string, memberTwoId: string) => {
+    try {
+        return await db.conversation.create({
+            data: {
+                memberOneId,
+                memberTwoId,
+            },
+            include: {
+                memmberOne: {
+                    include: {
+                        profile: true
+                    }
+                },
+                memmberTwo: {
+                    include: {
+                        profile: true
+                    }
+                }
+            }
+        })
+    } catch {
+        return null
+    }
+}
+
+/**
+ * 获取特定成员对话|没有测创建一个新的对话
+ * @param memberOneId 
+ * @param memberTwoId 
+ * @returns any conversation
+ */
+export const getOrCreateCoversation = async (memberOneId: string, memberTwoId: string) => {
+    let conversation = await findCoversation(memberOneId, memberTwoId) || await findCoversation(memberTwoId, memberOneId)
+
+    if (!conversation) {
+        conversation = await createNewCoversation(memberOneId, memberTwoId)
+    }
+
+    return conversation
+}
