b43c7fd0d50a64ede5540031801f51a0706285fc
Alicehhhmm <1245500390@qq.com>
2024-11-27 01:13:34 +0800

    feat: add socket.io 🎊

--- typings/type.ts
+++ typings/type.ts
@@ -1,6 +1,17 @@
 
+import { NextApiResponse } from "next";
+import { Server as SocketServer } from 'socket.io'
+import { Server as NetServer, Socket } from 'node:net'
 import { Server, Member, Profile } from '@prisma/client'
 
 export type ServerWithMembersWithProfiles = Server & {
     members: (Member & { profile: Profile })[];
+}
+
+export type NextApiResponseServerIo = NextApiResponse & {
+    Socket: Socket & {
+        server: NetServer & {
+            io: SocketServer
+        }
+    }
 }
