271be9b42604181e9517c33375a4826c8450ce06
Alicehhhmm <1245500390@qq.com>
2024-11-28 22:44:29 +0800

    fix: add EmojiPicker 表情包组件🎉

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -11,6 +11,7 @@ import { Plus, Smile } from 'lucide-react'
 import { Input } from '@/components/ui/input'
 import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
 import { useModal } from '@/hooks/use-modal-store'
+import { EmojiPicker } from '@/components/emoji-picker'
 
 interface ChatInputProps {
     apiUrl: string
@@ -84,7 +85,7 @@ export const ChatInput = ({ apiUrl, query, name, type }: ChatInputProps) => {
                                         {...field}
                                     />
                                     <div className='absolute top-7 right-8'>
-                                        <Smile />
+                                        <EmojiPicker onChange={(emoji: string) => field.onChange(`${field.value} ${emoji}`)} />
                                     </div>
                                 </div>
                             </FormControl>
